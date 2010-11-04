575a8ac04c3e63a4860b67ed6cca8b71f107cf65
Mathijs de Bruin <mathijs@mathijsfietst.nl>
2010-11-04 09:11:44 +0100

    It is perhaps better to start simple.

--- webshop/core/models.py
+++ webshop/core/models.py
@@ -16,3 +16,36 @@
 # along with this program; if not, write to the Free Software Foundation,
 # Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
 
+from django.db import models
+
+class ProductBase(models.Model):
+    """ Abstract base model for products. """
+
+    class Meta:
+        abstract = True
+
+    def get_price(self, *args, **kwargs):
+        """ Get price for the current product.
+            
+            This method _should_ be implemented in a subclass. """
+
+        raise Exception('Not implemented')
+
+class CartBase(models.Model):
+    """ Abstract base class for shopping carts. """
+
+    class Meta:
+        abstract = True
+
+class OrderBase(models.Model):
+    """ Abstract base class for orders. """
+
+    class Meta:
+        abstract = True
+
+class PaymentBase(models.Model):
+    """ Abstract base class for payments. """
+
+    class Meta:
+        abstract = True
+
