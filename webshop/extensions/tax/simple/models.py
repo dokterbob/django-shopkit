60785902840125e3f93dd1426fff60be726228d6
Mathijs de Bruin <mathijs@mathijsfietst.nl>
2010-11-06 13:39:44 +0100

    Start of a very basic tax module.

---
+++ webshop/extensions/tax/simple/models.py
@@ -0,0 +1,31 @@
+# Copyright (C) 2010-2011 Mathijs de Bruin <mathijs@mathijsfietst.nl>
+#
+# This file is part of django-webshop.
+#
+# django-webshop is free software; you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as published by
+# the Free Software Foundation; either version 2, or (at your option)
+# any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with this program; if not, write to the Free Software Foundation,
+# Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+
+from webshop.core.models import ProductBase
+
+from webshop.extensions.tax.simple import settings
+
+class TaxedProductBase(ProductBase):
+    """ A simple taxed product based on a configurable percentage. """
+
+    def get_taxes(self, *args, **kwargs):
+        """ Calculate tax according to a fixed percentage of the price. """
+
+        price = self.get_price(*args, **kwargs)
+        
+        return settings.TAX_PERCENTAGE * price
