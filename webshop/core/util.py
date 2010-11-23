411b594c7d363f68555a97fccff92a43392d0d04
Mathijs de Bruin <mathijs@mathijsfietst.nl>
2010-11-23 17:25:24 +0100

    Make sure we have actually looked up a model in get_model_from_string.

--- webshop/core/util.py
+++ webshop/core/util.py
@@ -16,14 +16,23 @@
 # along with this program; if not, write to the Free Software Foundation,
 # Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
 
-from django.db.models import get_model
+import logging
+logger = logging.getLogger(__name__)
+
+from django.db import models
+
 
 """ Several util functions for use in core functionality. """
+
 
 def get_model_from_string(model):
     """ 
     Takes a string in the form of `appname.Model`, (ie.
     `basic_webshop.CartItem`) and returns the model class for it.
     """
+    model_class = models.get_model(*model.split('.'))
     
-    return get_model(*model.split('.'))
+    assert isinstance(model_class, models.base.ModelBase), \
+        '%s does not refer to a known Model class.' % model
+
+    return model_class
