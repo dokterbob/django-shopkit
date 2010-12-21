585bdfd8f7a7c11664683936b83f8d2656aba82a
Mathijs de Bruin <mathijs@mathijsfietst.nl>
2010-12-21 14:01:29 +0100

    In the process of refactoring all explicit references to the product model out of many of the price base models.

--- webshop/extensions/price/advanced/models.py
+++ webshop/extensions/price/advanced/models.py
@@ -26,17 +26,15 @@ from webshop.core.util import get_model_from_string
 from webshop.core.settings import PRODUCT_MODEL
 from webshop.core.basemodels import QuantizedItemBase
 from webshop.extensions.price.models import PricedItemBase
-from webshop.extensions.price.advanced.settings import *
 
 
 class PriceBase(PricedItemBase):
     """ Abstract base class for price models, exposing a method to get the 
-        cheapest price under given conditions. 
+        cheapest price under given conditions.
+        
+        Be sure to add the proper `unique_together` contraints to subclasses
+        implementing an actual price model. 
     """
-
-    product = models.ForeignKey(PRODUCT_MODEL, db_index=True,
-                                verbose_name=_('product'))
-    """ Product this price relates to. """
     
     class Meta(PricedItemBase.Meta):
         abstract = True
@@ -49,31 +47,63 @@ class PriceBase(PricedItemBase):
             'FIXME: No prices found. This will raise an exception that should be called.'
 
         return qs.aggregate(models.Min('price'))['price__min']
-        
+    
+    
     @classmethod
-    def get_cheapest(cls, product, **kwargs):
-        """ Get the cheapest available price for the current product under
+    def get_cheapest(cls, **kwargs):
+        """ Get the cheapest available price under
             given conditions. """
         
-        valid = cls.get_valid_prices(product, **kwargs)
+        valid = cls.get_valid_prices(**kwargs)
         
         return cls._get_minimal_price(valid)
     
+    
     @classmethod
-    def get_valid_prices(cls, product, **kwargs):
-        """ Get valid prices under the given conditions. This should be
-            overriden for subclasses actually implementing some features.
+    def get_valid_prices(cls, **kwargs):
+        """ 
+        Get valid prices (as a QuerySet), given certain constraints. By
+        default, this returns all prices available. Where applicable,
+        subclasses might filter this result by:
+            
+            * Product
+            * Date
+            * Quantity
         """
 
-        raise NotImplementedError
+        return cls.objects.all()
 
 
-class DateRangedPriceBase(PriceBase):
-    """ Base class for a price that is only valid within a given date range. """
+class ProductPriceMixin(models.Model):
+    """ Represents prices available for a specific product product. """
+    
 
     class Meta(PriceBase.Meta):
         abstract = True
-        unique_together = ('product', 'start_date', 'end_date')
+
+    product = models.ForeignKey(PRODUCT_MODEL, db_index=True,
+                                verbose_name=_('product'))
+    """ Product this price relates to. """
+    
+    @classmethod
+    def get_valid_prices(cls, product, *args, **kwargs):
+        """ Return valid prices for a specified product """
+
+        valid = \
+            super(ProductPriceBase, self).get_valid_prices(*args, **kwargs)
+        
+        valid = valid.filter(product=product)
+        
+        return valid
+
+
+class DateRangedPriceMixin(models.Model):
+    """ Base class for a price that is only valid within a given date range. 
+    """
+
+    class Meta(PriceBase.Meta):
+        abstract = True
+        # unique_together = ('product', 'start_date', 'end_date')
 
     start_date = models.DateField(verbose_name=_('start date'),
                                   db_index=True)
@@ -81,62 +111,67 @@ class DateRangedPriceBase(PriceBase):
                                 db_index=True)
     
     @classmethod
-    def get_valid_prices(cls, product, date=None, *args, **kwargs):
+    def get_valid_prices(cls, date=None, *args, **kwargs):
         """ Return valid prices for a specified date, taking the current
             date if no date is specified. """
+
+        valid = \
+            super(DateRangedPriceBase, self).get_valid_prices(*args, **kwargs)
 
         # If no date is set, take today.
         if not date:
             date = datetime.datetime.today()
         
         # First get valid prices for the current situation
-        valid = cls.filter(product=product, 
+        valid = valid.filter(product=product, 
                            start_date__gte=date, 
                            end_date__lte=date)
         
         return valid
 
 
-class QuantifiedPriceBase(PriceBase, QuantizedItemBase):
+class QuantifiedPriceMixin(QuantizedItemBase):
     """ Base class for a price that is only valid above a certain quantity.
     """
 
     class Meta(PriceBase.Meta):
         abstract = True
-        unique_together = ('product', 'quantity')
+        # unique_together = ('product', 'quantity')
 
     @classmethod
-    def get_valid_prices(cls, product, quantity=1, *args, **kwargs):
+    def get_valid_prices(cls, quantity=1, *args, **kwargs):
         """ Get valid prices for a given quantity of items. If no
             quantity is given, 1 is assumed. 
         """
+        
+        valid = \
+            super(QuantifiedPriceBase, self).get_valid_prices(*args, **kwargs)
                 
         # First get valid prices for the current situation
-        valid = cls.filter(product=product, 
-                           quantity__gte=quantity)
+        valid = valid.filter(quantity__gte=quantity)
         
         return valid
 
 
-class PricedProductBase(models.Model):
-    """ Abstract base class for an advanced priced product. 
-        This base class allows for more complex pricing of articles, it 
-        enables features like:
-        
-        * Returning and storing different prices depending on a date range.
-        * Returning and storing different prices depending on the quantity.
-    """
-    
-    class Meta:
-        abstract = True
-    
-    def get_price(self, **kwargs):
-        """ Get the cheapest available price, based
-            upon WEBSHOP_PRICE_MODEL. """
-        
-        model = get_model_from_string(PRICE_MODEL)
-        cheapest = model.get_cheapest(**kwargs)
-        
-        return cheapest.get_price(**kwargs)
-
-
+# class PricedItemBase(models.Model):
+#     """ Abstract base class for an advanced priced product. 
+#         This base class allows for more complex pricing of articles, it 
+#         enables features like:
+#         
+#         * Returning and storing different prices depending on a date range.
+#         * Returning and storing different prices depending on the quantity.
+#     """
+#     
+#     
+#     class Meta:
+#         abstract = True
+#     
+#     def get_price(self, **kwargs):
+#         """ Get the cheapest available price, based
+#             upon WEBSHOP_PRICE_MODEL. """
+#         
+#         cheapest = self.__class__.get_cheapest(**kwargs)
+#         
+#         return cheapest.get_price(**kwargs)
+# 
+# 
