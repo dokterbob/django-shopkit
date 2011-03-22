825af6d0b31755abfe97e95c4b8155630ab95841
Mathijs de Bruin <mathijs@mathijsfietst.nl>
2011-03-22 11:33:35 +0100

    Buggy bug

--- webshop/extensions/discounts/advanced/models/__init__.py
+++ webshop/extensions/discounts/advanced/models/__init__.py
@@ -16,7 +16,5 @@
 # along with this program; if not, write to the Free Software Foundation,
 # Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
 
-from webshop.extensions.discounts.models import *
-
 from webshop.extensions.discounts.advanced.models.discount_models import *
 from webshop.extensions.discounts.advanced.models.order_models import *
