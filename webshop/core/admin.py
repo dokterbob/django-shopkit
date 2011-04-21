b5464bc7e07d463883f8dafbb87b21a654908117
Mathijs de Bruin <mathijs@mathijsfietst.nl>
2011-04-21 20:20:35 +0200

    Backport of previously shop-specific admin code.

---
+++ webshop/core/admin.py
@@ -0,0 +1,30 @@
+from django.utils.translation import ugettext_lazy as _
+from django.contrib import admin
+
+from webshop.core.utils import get_model_from_string
+from webshop.core.settings import ORDERSTATE_CHANGE_MODEL
+
+
+class PricedItemAdminMixin(object):
+    """ Admin mixin for priced items. """
+    def get_price(self, obj):
+        price = obj.get_price()
+        return price
+    get_price.short_description = _('price')
+
+
+class OrderStateChangeInline(admin.TabularInline):
+    model = get_model_from_string(ORDERSTATE_CHANGE_MODEL)
+
+    fields = ('date', 'state', )
+    readonly_fields = ('date', 'state')
+
+    extra = 0
+    max_num = 0
+    can_delete = False
+
+
+class OrderItemInlineBase(admin.TabularInline):
+    extra = 0
+
+    readonly_fields = ('price', )
